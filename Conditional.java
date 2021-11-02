ac0c8a865cb6f5ae4c0d7222c5706d079322d30e
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2021-11-02 12:18:22 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Conditional {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+
+        if(n1==n2)
+        {
+            System.out.println(n1 + " is equal to " + n2);
+        }
+        else if(n1>n2)
+            {
+                System.out.println(n1 + " is greater than " + n2);
+            }
+            else
+            System.out.println(n1 + " is smaller than " + n2);
+        
+    }
+}
