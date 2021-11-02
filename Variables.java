50389e724b7a1dbefea68ad7e9fd5753dd2c8a1a
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2021-11-02 12:17:37 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Variables {
+    public static void main(String[] args) {
+        int x=15;
+        int y=10;
+        int sum=x+y;
+        System.out.println("Sum of " + x + " and " + y + " is " +sum); //sum
+        int prd=x*y;
+        System.out.println("Product of " + x + " and " + y + " is " +prd); //product
+
+        // x/y  y/x  x%y
+        int v1 = x/y;
+        int v2 = y/x;
+        int v3 = x%y;
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+
+        //exp
+        int exp = (x*y)/(x+y);
+        System.out.println(exp);
+    }
+} 
