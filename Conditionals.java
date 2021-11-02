771f154a6a04ef4055e66ae564bdc5e5b8ac8a8a
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2021-11-02 12:17:56 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Conditionals {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x % 2 == 0)
+        {
+           System.out.println(x + " is even ");
+        }
+        else
+        {
+           System.out.println(x + " is odd");
+        }
+       System.out.println("Hardwork is better than smart work");
+    }
+    
+}
